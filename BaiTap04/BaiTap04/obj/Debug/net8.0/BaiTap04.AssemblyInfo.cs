89ab92db08aabc3c868aff3e3b76b65b5542f00e
NguyenThanhBuu <buu57633@gmail.com>
2024-09-10 08:20:20 +0700

    Cập nhật dự án Baitap06

--- BaiTap04/BaiTap04/obj/Debug/net8.0/BaiTap04.AssemblyInfo.cs
+++ BaiTap04/BaiTap04/obj/Debug/net8.0/BaiTap04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ccc48ede83ca1bd98298b988fd57b9dea54fc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e347d772f9cd967d833af890d7cab8b73846f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
