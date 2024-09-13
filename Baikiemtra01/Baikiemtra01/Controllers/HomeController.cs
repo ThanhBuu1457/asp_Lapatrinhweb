49815ceed288c0fe0dcb6b5f5c2d921f214f236a
ThanhBuu1457 <buu57633@gmail.com>
2024-09-14 04:31:52 +0700

    Update HomeController.cs

--- Baikiemtra01/Baikiemtra01/Controllers/HomeController.cs
+++ Baikiemtra01/Baikiemtra01/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Baikiemtra01.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BaiTap06.Controllers
+namespace Baikiemtra01.Controllers
 {
     public class HomeController : Controller
     {
