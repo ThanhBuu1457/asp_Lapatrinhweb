ba50f94de8702f52447ca8c3c8b4138695467a2a
NguyenThanhBuu <buu57633@gmail.com>
2024-09-14 04:45:29 +0700

    gitbub loi

--- Baitap06/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Baitap06.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
